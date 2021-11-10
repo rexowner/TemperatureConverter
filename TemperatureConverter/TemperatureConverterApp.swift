aee26cb66a22294f444577fd06448cbb263ee76a
Les Poltrack <lapwack@gmail.com>
2021-11-10 14:01:08 -0800

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Les Poltrack on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
